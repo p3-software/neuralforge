5e3d7d7e06b492dbaacd57bc2e83bed3175eeaf9
Enrique Alpízar <ealpizr@gmail.com>
2025-04-03 22:01:44 -0600

    feat: add entities for teaching project course scheduling

--- neuralforge_api/src/main/java/com/cenfotec/p3/neuralforge_api/model/entity/TeachingProjectEntity.java
+++ neuralforge_api/src/main/java/com/cenfotec/p3/neuralforge_api/model/entity/TeachingProjectEntity.java
@@ -7,7 +7,9 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Entity class representing a teaching project in the system.
@@ -29,15 +31,36 @@ public class TeachingProjectEntity extends ProjectEntity {
         super.setProjectType(ProjectTypeEnum.TEACHING);
     }
 
-    @OneToOne(cascade = CascadeType.ALL)
+    /**
+     * The days of the week when classes will be held.
+     */
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "selected_days_id", referencedColumnName = "id")
     private SelectedDaysEntity selectedDays;
 
+    /**
+     * Number of hours per class session.
+     */
     private Integer dailyHours;
 
+    /**
+     * Total number of weeks for this teaching project.
+     */
     private Integer weeksCount;
 
+    /**
+     * Date when the course starts.
+     */
     private Date startDate;
 
+    /**
+     * Date when the course ends.
+     */
     private Date endDate;
+    
+    /**
+     * List of weeks in this teaching project's schedule.
+     */
+    @OneToMany(mappedBy = "teachingProject", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CourseWeekEntity> weeks = new ArrayList<>();
 } 
